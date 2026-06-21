846e3d09beaf4e01ae3f21e8356fd4a89805bc9c
sathvik307393 <307393@ust.com>
2026-06-21 14:57:13 +0530

    style: fix terraform formatting

--- terraform/modules/acr/main.tf
+++ terraform/modules/acr/main.tf
@@ -1,8 +1,8 @@
 resource "azurerm_container_registry" "this" {
 
-  name                = var.name
-  resource_group_name = var.resource_group_name
-  location            = var.location
+  name                          = var.name
+  resource_group_name           = var.resource_group_name
+  location                      = var.location
   sku                           = var.sku
   admin_enabled                 = var.admin_enabled
   public_network_access_enabled = false
